583f80c295f837355b40e724a277edbc878ac7cf
Truls Johan Jacobsen-Lamøy <truls@Trulss-MacBook-Pro.local>
2026-03-16 17:38:21 +0100

    chore: added enums for categorys, condition and listing status

---
+++ Arbeidskrav2/Enums.cs
@@ -0,0 +1,26 @@
+namespace Arbeidskrav2
+{
+    public enum Enums
+    {
+        Electronics,
+        ClothingAndAccessories,
+        FurnitureAndHome,
+        BooksAndMedia,
+        SportsAndOutdoors,
+        Other,
+    }
+
+    public enum Condition
+    {
+        New,
+        LikeNew,    
+        Good,       
+        Fair,
+    }
+
+    public enum ListingStatus
+    {
+        Available,
+        Sold,
+    }
+}
